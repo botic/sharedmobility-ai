c6eb13d3f93676b431922b89d88609ceb27e7e51
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2019-05-18 12:03:12 +0200

    Adds avg count per station per week to query.

--- analytics/database/report-by-week.sql
+++ analytics/database/report-by-week.sql
@@ -1,6 +1,7 @@
 SELECT
     date_trunc('week', snp_timestamp) AS "Week",
     count(*) AS "Snapshots",
+    to_char(cast(sum(snp_vehicles_available) as decimal) / count(*), '990.00') AS "Bikes per Station",
     sum(snp_vehicles_available) AS "Vehicles Available",
     sum(snp_vehicles_faulty) AS "Vehicles Faulty",
     sum(snp_boxes_available) AS "Boxes Available",
