60256fd9ac7b057b338dd0d68b4072a7250802ba
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2019-09-02 01:21:26 +0200

    Adds prediction API

---
+++ web-api/src/utils.js
@@ -0,0 +1,122 @@
+const flat = require("array.prototype.flat");
+const config = require("./config");
+
+const HOLIDAYS = config.get("holidays");
+
+exports.toInputArray = function(dt, rain, sunshine, temperature) {
+    const holiday = HOLIDAYS.indexOf(dt.toFormat("dd.MM.yyyy")) >= 0 ? 1 : 0;
+
+    const dayCategory = new Array(7).fill(0);
+    dayCategory[dt.weekday - 1] = 1;
+
+    const hourCategory = new Array(24).fill(0);
+    hourCategory[dt.hour] = 1;
+
+    const minuteCategory = new Array(4).fill(0);
+    if (dt.minute >= 0 && dt.minute < 15) {
+        minuteCategory[0] = 1;
+    } else if (dt.minute >= 15 && dt.minute < 30) {
+        minuteCategory[1] = 1;
+    } else if (dt.minute >= 30 && dt.minute < 45) {
+        minuteCategory[2] = 1;
+    } else if (dt.minute >= 45 && dt.minute < 60) {
+        minuteCategory[3] = 1;
+    }
+
+    const rainCategory = new Array(4).fill(0);
+    if (rain >= 1) {
+        rainCategory[0] = 1;
+    }
+    if (rain >= 2.5) {
+        rainCategory[1] = 1;
+    }
+    if (rain >= 5) {
+        rainCategory[2] = 1;
+    }
+    if (rain >= 9) {
+        rainCategory[3] = 1;
+    }
+
+    const sunshineCategory = new Array(4).fill(0);
+    if (sunshine >= 25) {
+        sunshineCategory[0] = 1;
+    }
+    if (sunshine >= 50) {
+        sunshineCategory[1] = 1;
+    }
+    if (sunshine >= 75) {
+        sunshineCategory[2] = 1;
+    }
+    if (sunshine === 100) {
+        sunshineCategory[3] = 1;
+    }
+
+    const temperatureCategory = new Array(4).fill(0);
+    if (temperature >= 15) {
+        temperatureCategory[0] = 1;
+    }
+    if (temperature >= 20) {
+        temperatureCategory[1] = 1;
+    }
+    if (temperature >= 25) {
+        temperatureCategory[2] = 1;
+    }
+    if (temperature >= 30) {
+        temperatureCategory[3] = 1;
+    }
+
+    return flat([
+        holiday,
+        dayCategory,
+        hourCategory,
+        minuteCategory,
+        rainCategory,
+        sunshineCategory,
+        temperatureCategory
+    ], 1);
+};
+
+const THRESHOLD = 0.8;
+exports.humanReadableMessage = function(prediction) {
+    if (prediction.filter(val => val >= THRESHOLD).lenght > 1) {
+        return "indecisive";
+    }
+
+    const messages = [];
+
+    if (prediction[0] >= THRESHOLD) {
+        messages.push("full of bikes (> 80%)");
+
+        if (prediction.slice(1).some(val => val >= 0.5)) {
+            messages.push("very uncertain");
+        } else if (prediction.slice(1).some(val => val >= 0.3)) {
+            messages.push("uncertain");
+        }
+    } else if (prediction[1] >= THRESHOLD) {
+        messages.push("bike count 50 % to 80 %");
+
+        if (prediction.filter((val, index) => index !== 1).some(val => val >= 0.5)) {
+            messages.push("very uncertain");
+        } else if (prediction.filter((val, index) => index !== 1).some(val => val >= 0.3)) {
+            messages.push("uncertain");
+        }
+    } else if (prediction[2] >= THRESHOLD) {
+        messages.push("bike count 20 % to 50 %");
+
+        if (prediction.filter((val, index) => index !== 2).some(val => val >= 0.5)) {
+            messages.push("very uncertain");
+        } else if (prediction.filter((val, index) => index !== 2).some(val => val >= 0.3)) {
+            messages.push("uncertain");
+        }
+    } else if (prediction[3] >= THRESHOLD) {
+        messages.push("bikes low (< 20%)");
+
+        if (prediction.slice(0, 3).some(val => val >= 0.5)) {
+            messages.push("very uncertain");
+        } else if (prediction.slice(0, 3).some(val => val >= 0.3)) {
+            messages.push("uncertain");
+        }
+    }
+
+    return messages.join(", ");
+};
--- web-api/src/v1/index.js
+++ web-api/src/v1/index.js
@@ -1,11 +1,18 @@
 const config = require("../config");
 
-const express = require("express");
-const { param, validationResult } = require("express-validator");
+const axios = require("axios");
+const { DateTime } = require("luxon");
+const tf = require("@tensorflow/tfjs-node");
+const { Storage } = require("@google-cloud/storage");
 
+const express = require("express");
+const { param, query, validationResult } = require("express-validator");
 const app = module.exports = express();
 
-const { Storage } = require("@google-cloud/storage");
+const {
+    toInputArray,
+    humanReadableMessage
+} = require("../utils");
 
 app.get("/", (req, res) => res.status(200).send({
     "status": "okay",
@@ -21,13 +28,120 @@ app.get("/models/:service/:station", [
         return res.status(400).json({ errors: errors.array() });
     }
 
-
-    const prefix = `tfjs-models/service_${req.params.service}/station_${req.params.station}/`;
+    const prefix = `service_${req.params.service}/station_${req.params.station}/`;
 
     const storage = new Storage();
     const [files] = await storage.bucket(config.get("storage:bucket")).getFiles({
         prefix
     });
 
-    res.status(200).send(files.map(f => f.name).filter(name => name !== prefix && name.endsWith("/")));
+    res.status(200).send(files
+        .map(f => f.name)
+        .filter(name => name !== prefix && name.endsWith("/"))
+        .map(name => name.match(/\/([^/]+)\/?$/)[1])
+    );
+});
+
+app.get("/predict/:service/:station", [
+    param("service").isInt({ min: 1, max: config.get("smai:maxServiceId") }),
+    param("station").isInt({ min: 1, max: config.get("smai:maxStationId") }),
+    query("model").optional().isString().matches(/[\w_-]{1,255}/i)
+], async (req, res) => {
+    res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.set("Pragma", "no-cache");
+    res.set("Expires", "0");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const modelName = req.query.model || "best";
+    const filePath = `service_${req.params.service}/station_${req.params.station}/${modelName}/`;
+
+    const storage = new Storage();
+    const bucket = storage.bucket(config.get("storage:bucket"));
+    const file = bucket.file(filePath);
+
+    const exists = (await file.exists())[0];
+    if (!exists) {
+        return res.status(404).json({ errors: [{ msg: `Not found: ${modelName}` }] });
+    }
+
+    const isPublic = (await file.isPublic())[0];
+    if (!isPublic) {
+        return res.status(403).json({ errors: [{ msg: `Forbidden.` }] });
+    }
+
+    let rain = 0,
+        sunshine = 50,
+        temperature = 25;
+
+    try {
+        const zamgTawes = await axios.get(config.get("zamg:ogd"));
+        if (zamgTawes.status === 200) {
+            const zamgData = zamgTawes.data
+                .replace(/\r\n/g, "\n")
+                .split("\n")
+                .filter(line => line !== "")
+                .map(line => line.split(";")
+                    .map(cell => cell.replace(/^"/, "").replace(/"$/, ""))
+                );
+
+            const synop = config.get("zamg:synop")["service" + req.params.service];
+            if (synop) {
+                const headers = zamgData[0].map(header => header.toLowerCase());
+
+                const tempIndex = headers.findIndex(val => val === "t °c");
+                const rainIndex = headers.findIndex(val => val === "n l/m²");
+                const sunIndex = headers.findIndex(val => val === "so %");
+
+                const line = zamgData.find(line => line[0] === synop);
+                temperature = Number.parseFloat(line[tempIndex].replace(",", "."));
+                sunshine = Number.parseInt(line[sunIndex]);
+                rain = Number.parseFloat(line[rainIndex].replace(",", "."));
+
+                if (isNaN(rain)) {
+                    rain = 0;
+                }
+
+                if (isNaN(sunshine)) {
+                    sunshine = 50;
+                }
+
+                if (isNaN(temperature)) {
+                    temperature = 25;
+                }
+            } else {
+                console.warn("No synop mapping found for service " + req.params.service);
+            }
+        }
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ errors: [{ msg: `Weather data not available!` }] });
+    }
+
+    const publicURL = `https://storage.googleapis.com/${config.get("storage:bucket")}/${filePath}`;
+    try {
+        const model = await tf.loadLayersModel(publicURL + "model.json");
+
+        const dt = DateTime.utc();
+        const prediction2d = await model.predict(tf.tensor2d([
+            toInputArray(dt, 0, 100, 20),
+            toInputArray(dt.plus({ minutes: 15 } ), rain, sunshine, temperature),
+            toInputArray(dt.plus({ minutes: 30 } ), rain, sunshine, temperature),
+            toInputArray(dt.plus({ minutes: 45 } ), rain, sunshine, temperature),
+            toInputArray(dt.plus({ minutes: 60 } ), rain, sunshine, temperature),
+        ]));
+        const predictions = await prediction2d.array();
+        return res.status(200).json(predictions.map(prediction => {
+            return {
+                message: humanReadableMessage(prediction),
+                prediction
+            }
+        }));
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ errors: [{ msg: `Internal Server Error.` }] });
+    }
 });
